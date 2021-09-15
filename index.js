144e676c1a51aaed23e04f7b54391c1da8dc6839
Harmun <harmun.rossouw@coil.com>
2021-09-15 14:27:51 +0200

    able to receive search results

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hi there!');
+// API KEY
+// 32c267b5
+
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: { apikey: "32c267b5", s: searchTerm },
+  });
+  return response.data.Search;
+};
+
+const input = document.querySelector("input");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+};
+
+// ensure that whenever input is detected, it is run through the debounce shield -> which runs onInput
+input.addEventListener("input", debounce(onInput, 500));
