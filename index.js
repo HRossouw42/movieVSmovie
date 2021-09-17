83dda14a9e05e01a2594fbfc3ea058cb220b52ed
Harmun <harmun.rossouw@coil.com>
2021-09-17 16:26:20 +0200

    s

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get("https://www.omdbapi.com/", {
     params: { apikey: "32c267b5", i: movie.imdbID },
   });
 
